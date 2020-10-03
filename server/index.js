5fcd0c58d16aa6c5ce404024f92396cf9bc659b6
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-03 12:52:15 +0600

    setup node server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
