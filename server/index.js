6770532fef8925dec6f088b1caa16b287c0ecc44
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-03 13:35:20 +0600

    add Registration Info to the mongoDb server

--- server/index.js
+++ server/index.js
@@ -1,14 +1,31 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fgaci.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
-const port = 5000
+app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const registrations = client.db("volunteerNetwork").collection("volunteerRegistrations");
+  
+  app.post('/addRegistration', (req,res) => {
+    const newRegistration = req.body;
+    registrations.insertOne(newRegistration)
+    .then(result => {
+      res.send(result.insertedCount > 0)
+    })
+  })
+});
+
+
+app.listen(5000, () => {
+  console.log(`Example app listening at http://localhost:5000`)
 })
